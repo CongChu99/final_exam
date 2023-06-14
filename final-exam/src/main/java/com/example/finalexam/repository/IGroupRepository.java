66a77a53a8fe2978625432b9181ecc0ca9b3a300
Cong Chu <92419576+CongChu99@users.noreply.github.com>
2023-06-15 01:42:52 +0700

    sad

--- final-exam/src/main/java/com/example/finalexam/repository/IGroupRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.finalexam.repository;
-
-import com.example.finalexam.entity.Group;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IGroupRepository extends JpaRepository<Group, Integer> {
-}
